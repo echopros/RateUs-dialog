a18148c2cbd63e0ba6fd33e0b3f39bb11baedc90
Anton Kizema <anton_kizema@mail.ru>
2017-12-13 19:07:37 +0200

    graphics

--- app/src/main/java/ratedialogexample/ratedialogexample/MainActivity.java
+++ app/src/main/java/ratedialogexample/ratedialogexample/MainActivity.java
@@ -1,7 +1,8 @@
 package ratedialogexample.ratedialogexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import rateusdialoghelper.RateDialogHelper;
 
@@ -12,10 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RateDialogHelper dailog = new RateDialogHelper.Builder()
-                .setShowEveryTime(true)
-                .build();
+        int colorInactive = Color.parseColor("#c59d23");
+        int colorActive = Color.parseColor("#f8c017");
 
-        dailog.showRateDialog(this);
+        RateDialogHelper.Builder rateDialogHelperbuilder = new RateDialogHelper.Builder()
+                .setRatingColorActive(colorActive)
+                .setRatingColorInactive(colorInactive)
+                .setTitleAppNameColor(colorActive)
+                .setCancelColor(colorInactive)
+                .setRateColor(colorActive)
+                .setFeedbackEmail("someamil@gmail.com");
+
+        if (BuildConfig.DEBUG){
+            //for debug show rate dlg all the time
+            rateDialogHelperbuilder.setShowEveryTime(true);
+        }
+
+        rateDialogHelperbuilder.build().showRateDialog(this);
     }
 }
